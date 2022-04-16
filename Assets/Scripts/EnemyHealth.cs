45a6052a2f613fb881b1bae17962c6870ab95947
tixvage <mel10h3421@gmail.com>
2022-04-16 16:19:58 +0300

    typo

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealthManager : MonoBehaviour
+public class EnemyHealth : MonoBehaviour
 {
     Enemy _enemy;
 
