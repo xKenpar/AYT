4cedeadbd414da6b01f449ab0469f808075a0fb4
MFaik <m.faikekclr@gmail.com>
2022-04-17 15:35:34 +0300

    Order Manager + new Letters spawn + new Letter Sprites

--- Assets/Scripts/LetterManager.cs
+++ Assets/Scripts/LetterManager.cs
@@ -17,18 +17,24 @@ public class LetterManager : MonoBehaviour
         Dash,
     }
 
+    [SerializeField] List<GameObject> LetterPrefabs;
+
+    [SerializeField] List<Sprite> PreviewSprites;
+    [SerializeField] SpriteRenderer PreviewRenderer;
+
+    [SerializeField] Transform PreviewBar;
+
+    [SerializeField] BoxCollider2D Deck;
+
     [SerializeField] List<GameObject> RecycleBars; 
     int _recycleProgress = 0;
 
     [SerializeField] GameObject NewSlotEffect;
 
     [SerializeField] List<GameObject> LockedSlots;
+    [SerializeField] List<GameObject> LockIcons;
     int _currentSlot = 0;
 
-    [SerializeField] List<Sprite> PreviewSprites;
-    [SerializeField] SpriteRenderer PreviewRenderer;
-
-    [SerializeField] Transform PreviewBar;
 
     LetterEnum _currentPreview;
     float _previewProgress = 0;
@@ -37,6 +43,31 @@ public class LetterManager : MonoBehaviour
         foreach(var bar in RecycleBars)
             bar.SetActive(false);
         PreviewBar.localScale = new Vector3(_previewProgress, 1, 1);
+
+        _currentPreview = (LetterEnum)Random.Range(0,15);
+        PreviewRenderer.sprite = PreviewSprites[(int)_currentPreview];
+
+        for(int i = 0;i < 3;i++)
+            SpawnNewLetter((LetterEnum)Random.Range(0,15));
+    }
+
+    public void EnemyKilled() {
+        _previewProgress += .2f;
+        if(_previewProgress >= 1){
+            _previewProgress = 0;
+
+            SpawnNewLetter(_currentPreview);
+            _currentPreview = (LetterEnum)Random.Range(0,15);
+            PreviewRenderer.sprite = PreviewSprites[(int)_currentPreview];
+        }
+
+        PreviewBar.localScale = new Vector3(_previewProgress, 1, 1);
+        PreviewBar.localPosition = new Vector3((1-_previewProgress)/2,0);
+    }
+
+    void SpawnNewLetter(LetterEnum newLetter) {
+        Instantiate(LetterPrefabs[(int)newLetter] ,
+                    new Vector3(Random.Range(Deck.bounds.min.x+.3f, Deck.bounds.max.x-.3f), Random.Range(Deck.bounds.min.y+.3f, Deck.bounds.max.y-.3f)), Quaternion.identity);
     }
 
     public void RecycleLetter() {
@@ -54,25 +85,9 @@ public class LetterManager : MonoBehaviour
     void OpenNewSlot() {
         if(_currentSlot >= LockedSlots.Count)
             return;
-        LockedSlots[_currentSlot++].SetActive(true);
-        Instantiate(NewSlotEffect, LockedSlots[_currentSlot-1].transform.position, Quaternion.identity);
-    }
-
-    public void EnemyKilled() {
-        _previewProgress += .2f;
-        if(_previewProgress >= 1){
-            _previewProgress = 0;
-
-            GetNewLetter(_currentPreview);
-            _currentPreview = (LetterEnum)Random.Range(0,15);
-            PreviewRenderer.sprite = PreviewSprites[(int)_currentPreview];
-        }
-
-        PreviewBar.localScale = new Vector3(_previewProgress, 1, 1);
-        PreviewBar.localPosition = new Vector3((1-_previewProgress)/2,0);
-    }
-
-    void GetNewLetter(LetterEnum newLetter) {
-
+        LockIcons[_currentSlot].SetActive(false);
+        LockedSlots[_currentSlot].SetActive(true);
+        Instantiate(NewSlotEffect, LockedSlots[_currentSlot].transform.position, Quaternion.identity);
+        _currentSlot++;
     }
 }
